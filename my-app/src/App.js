cb505de09dd6624bf7f5c2a4185f34578d1c8a59
sejalkataria <katariasejal18@gmail.com>
2022-12-16 16:18:39 +0530

    added code to change title when mode is changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,11 +23,13 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert('Dark mode is enabled', 'success')
+      document.title = 'TextUtils - Dark Mode'
     }
     else if (mode === 'dark') {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert('Light mode is enabled', 'success')
+      document.title = 'TextUtils - Light Mode'
     }
   }
   return (
