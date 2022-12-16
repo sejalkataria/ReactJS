7f34d2bef62f4af0a1c652f26a30a73de47efc83
sejalkataria <katariasejal18@gmail.com>
2022-12-16 17:05:42 +0530

    added routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About"
 import Alert from "./components/Alert"
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState('light') //whether dark mode is enabled or not
@@ -34,12 +35,16 @@ function App() {
   }
   return (
     <div>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm heading="Enter Text to analyze below" showAlert={showAlert} mode={mode} />
-        {/*<About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/" element={<TextForm heading="Enter Text to analyze below" showAlert={showAlert} mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
