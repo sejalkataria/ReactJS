9189a538e6d484eddb16316284cf8067efe08365
sejalkataria <katariasejal18@gmail.com>
2023-01-02 14:53:53 +0530

    added top loading bar

--- news-app/src/components/NewsItem.js
+++ news-app/src/components/NewsItem.js
@@ -6,13 +6,17 @@ export default class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0'
+          }}>
+            <span className=" badge rounded-pill bg-danger">{source}</span>
+          </div>
           <img style={{ height: '160px' }} src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}<span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-              style={{ left: '90%', zIndex: '1' }}>
-              {source}
-              <span className="visually-hidden">unread messages</span>
-            </span></h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">{description}</p>
             <p><small className="text-muted"> By {!author ? 'unknown' : author} {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read more</a>
